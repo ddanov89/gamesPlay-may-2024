ebe6d8beddcba7821d6988118457a65809b2a81f
ddanov89 <138501928+ddanov89@users.noreply.github.com>
2024-07-18 14:58:34 +0300

    Initialize project and as well as add react router

---
+++ README.md
@@ -0,0 +1,16 @@
+# gamesPlay-may-2024
+SoftUni React course project
+
+
+## 1. Initialize Project
+- [x] Initialize git repo
+- [x] Add back-end server
+- [x] Add base vite react project as client
+- [x] CleanUp client
+- [x] Add project resources
+- [x] Separate html into components
+## 2. React Router
+- [x] Install react-router-dom
+- [x] Setup react-router-dom
+- [x] Add routes in App.jsx
+- [x] Add links in the navigation
---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import GameList from "./components/game-list/GameList";
+import GameCreate from "./components/game-create/GameCreate";
+import GameEdit from "./components/game-edit/GameEdit";
+
+function App() {
+  return (
+    <>
+      <div id="box">
+        <Header />
+
+        <main id="main-content">
+          <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/games" element={<GameList />} />
+                <Route path="/game/create" element={<GameCreate />} />
+          </Routes>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
